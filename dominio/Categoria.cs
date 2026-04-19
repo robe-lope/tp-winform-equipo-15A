61b7e5727c21a879cad521eabce1e59b3acde98d
AMiltonPeralta <130006712+AMiltonPeralta@users.noreply.github.com>
2026-04-19 20:55:12 -0300

    Add files via upload

---
+++ dominio/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm
+{
+    internal class Categoria
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        // Igual que en Marca, esto facilitará mostrarla en listas desplegables
+        public override string ToString()
+        {
+            return Descripcion;
+        }
+    }
+}
