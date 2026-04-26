e098ebc2bd8ebd07769f91b054c65001a3def51c
nicoagurto19 <nicoagurto19@gmail.com>
2026-04-26 20:50:24 -0300

    Add files via upload

---
+++ dominio/Marca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Marca
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        public string toString()
+        {
+            return Descripcion;
+        }
+
+    }
+}
