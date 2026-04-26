bb62145c516819fd7598379f3e0f4cd99bf223ac
robe-lope <ezequiel.lopez@alumnos.frgp.utn.edu.ar>
2026-04-26 15:56:07 -0300

    clase Imagen y Service

---
+++ dominio/Imagen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Imagen
+    {
+        public int Id { get; set; }
+        public int IdArticulo { get; set; }
+        public string ImagenUrl { get; set; }
+
+    }
+}
